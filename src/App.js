cda761aef739048cccb1ad9fd964805f6cd0d683
abhishek8173 <abhishekpandey19112000@gmail.com>
2025-05-25 12:47:37 +0530

    finalised project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import './App.css';
+import {
+  Route, 
+  Routes
+} from 'react-router-dom';
+import pages from './utils/pages';
+import Layout from './components/Layout';
+import Home from './components/screens/Home';
+import Bookings from './components/screens/Booking';
+import ConfirmedBooking from './components/screens/Booking/ConfirmedBooking';
+import NotFound from './components/screens/NotFound';
+import UnderConstruction from './components/screens/ComingSoon';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Layout>
+        <Routes>
+          <Route path={pages.get('home').path} element={<Home />} />
+          <Route 
+            path={pages.get('about').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route 
+            path={pages.get('menu').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route path={pages.get('bookings').path} element={<Bookings />} />
+          <Route 
+            path={pages.get('confirmedBooking').path} 
+            element={<ConfirmedBooking />} 
+          />
+          <Route 
+            path={pages.get('orderOnline').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route 
+            path={pages.get('login').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </>
   );
-}
+};
 
 export default App;
